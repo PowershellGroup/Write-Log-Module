bddba586d32836d649bc30b3dbd2db8dec695a1e
Antony Bragg <antonybragg@gmail.com>
2022-01-18 20:55:25 +0000

    Create Write-Log.psm1
    Initial Commit.

---
+++ Write-Log.psm1
@@ -0,0 +1,57 @@
+<#
+.SYNOPSIS
+Basic log file creator.
+
+.DESCRIPTION
+Creates an entry of a specified severity into the log file.
+
+.OUTPUTS
+A *.log file with specific name or location (Default is script root named log.log)
+
+.PARAMETER Message
+The log message entered into the log file.
+
+.PARAMETER severity
+The severity set for the log message within the log file. (Info, Warning, Error)
+
+.PARAMETER logLocation
+The directory of the log file.
+
+.PARAMETER logName
+The name of the log file. Should be a *.log extension. The default location is the area the script was executed.
+
+.EXAMPLE
+write-log -Message "Oh no its all gone wrong"
+.EXAMPLE
+write-log -Message "Oh no its an error!" -severity "Error"
+.EXAMPLE
+write-log -Message "Oh no its all gone wrong" -severity "Error" -LogLocation "C:\temp\Log.log"
+
+.NOTES
+This original module can be found in the following GitHub Repo: https://github.com/captainqwerty/Write-Log
+#>
+
+Function Write-Log {
+  Param(
+      [Parameter(Position=0, Mandatory=$true, HelpMessage="Enter your log message.")]
+      [string]$Message,
+
+      [Parameter(Position=1, HelpMessage="Enter the log level severity between.")]
+      [ValidateSet("Info", "Warning", "Error")]
+      [string]$severity = "Info",
+
+      [Parameter(Position=2, HelpMessage="Directory for the log file.")]
+      [string]$logLocation = "$PSScriptRoot\log.log"
+  )
+
+  if(!test-path $logLocation) {
+    New-Item $logLocation -Force
+  }
+
+  $timeStamp = Get-date -Format "dd/MM/yyyy HH:mm:ss"
+  $Output = "$timeStamp - [$severity] $Message"
+  Add-Content $logLocation -value $Output
+  Write-Host "$Output"
+}
+
+Export-ModuleMember -Function Write-Log
